ed659c1d5fe58c3238202e9430202e437099b991
Netra-Patel-08 <pate1403@algonquinlive.com>
2025-05-13 10:37:12 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,29 @@
-alert ('Test the GH Pages');
+// Declared Variables
+let a = 3;
+let b = 5;
+let c;
+
+let output = `
+let a = 3;
+let b = 5;
+let c; 
+--------------
+
+a + b = ${a + b}
+a - b = ${a - b}
+a * b = ${a * b}
+a / b = ${a / b}
+a % b = ${a % b}
+a += b = ${a += b}
+a -= b = ${a -= b}
+a *= b = ${a *= b}
+a /= b = ${a /= b}
+a %= b = ${a %= b}
+a == b = ${a == b}
+a != b = ${a != b}
+a > b = ${a > b}
+a < b = ${a < b}
+!a && !c = ${!a && !c }
+!a || !c = ${!a || !c}
+`
+console.log (output)
